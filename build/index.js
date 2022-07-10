ba3f964813e8a2ff85ee82a8577300d47522babe
aaron <seungwoo.aaron@gmail.com>
2022-07-10 18:36:01 +0900

    :sparkles: simple blockchain

--- build/index.js
+++ build/index.js
@@ -1,8 +1,43 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
 class Block {
-    constructor(data) {
+    constructor(prevHash, height, data) {
+        this.prevHash = prevHash;
+        this.height = height;
         this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
     }
-    static hello() {
-        return "hi";
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return crypto_1.default.createHash("sha256").update(toHash).digest("hex");
     }
 }
+class BlockChian {
+    constructor() {
+        this.blocks = [];
+    }
+    getPrevHash() {
+        if (!this.blocks.length)
+            return "";
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    getLenght() {
+        return this.blocks.length + 1;
+    }
+    addBlock(data) {
+        const newBlock = new Block(this.getPrevHash(), this.getLenght(), data);
+        this.blocks.push(newBlock);
+    }
+    getAllBlocks() {
+        return [...this.blocks]; // avoid connecting directly to the state of blocks
+    }
+}
+const blockChain = new BlockChian();
+blockChain.addBlock("first");
+blockChain.addBlock("second");
+blockChain.addBlock("third");
+console.log(blockChain.getAllBlocks());
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,50 @@
-import { init, exit } from "./myPackage";
-init;
+import crypto from "crypto";
+interface Shape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements Shape {
+  public readonly hash: string;
+  constructor(
+    public readonly prevHash: string,
+    public readonly height: number,
+    public readonly data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
+
+class BlockChian {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (!this.blocks.length) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  private getLenght() {
+    return this.blocks.length + 1;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPrevHash(), this.getLenght(), data);
+    this.blocks.push(newBlock);
+  }
+  public getAllBlocks(): readonly Block[] {
+    return [...this.blocks]; // avoid connecting directly to the state of blocks
+  }
+}
+
+const blockChain = new BlockChian();
+blockChain.addBlock("first");
+blockChain.addBlock("second");
+blockChain.addBlock("third");
+
+console.log(blockChain.getAllBlocks());
